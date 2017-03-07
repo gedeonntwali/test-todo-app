a44771f15ec9898601c01071d415847ffabad40d
Gedeon Ntwali <ntwari28@gmail.com>
2017-03-07 10:17:12 +0200

    Worked on homework

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -14,4 +14,52 @@ RSpec.describe Task, type: :model do
       expect(task.complete).to eq(true)
     end
   end
+
+  describe '#toggle_favorite' do 
+    it 'should switch favorite to false if it began as true' do 
+      task = Task.new(favorite: true)
+      task.toggle_favorite!
+      expect(task.favorite).to eq(false)
+    end
+
+    it 'should switch favorite to true if it began as false' do 
+      task = Task.new(favorite: false)
+      task.toggle_favorite!
+      expect(task.favorite).to eq(true)
+    end
+  end
+
+  describe '#overdue' do 
+    it 'should show that the task is overdue' do 
+      task = Task.new(deadline: Time.now - 1.hour)
+      expect(task.overdue?).to eq(true)
+    end
+  end
+
+  describe 'increment_priority!' do
+    it 'should show that there is an incrementation of priority' do 
+      task = Task.new(priority: 7)
+      task.increment_priority!
+      expect(task.priority).to eq(8)
+    end
+  end
+
+  describe 'decrement_priority!' do
+    it 'should show that there is a decrement of priority' do 
+      task = Task.new(priority: 7)
+      task.decrement_priority!
+      expect(task.priority).to eq(6)
+    end
+  end
+
+  describe '#snooze_hour!' do 
+    it 'should' do 
+      task = Task.new(deadline: Time.now)
+      task.snooze_hour!
+      expect(task.deadline).to eq(Time.now - 1.hour)
+    end
+  end
+
+
+
 end
